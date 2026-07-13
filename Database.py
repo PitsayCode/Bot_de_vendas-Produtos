3a0ddcf4fa35680788938d8f388418d86ea980e9
Victor <victoralexandre608@gmail.com>
2026-07-13 18:33:41 -0300

    Reformulei a captura das credenciais e a abertura e fechamento do BD

--- Database.py
+++ Database.py
@@ -4,73 +4,79 @@ conn = sqlite3.connect('BancoDeDados.db')
 
 
 def criar_tabela_user():
-    cursor = conn.cursor()
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        last_name TEXT NOT NULL,
-        email TEXT NOT NULL UNIQUE,
-        senha TEXT NOT NULL,
-        dia INTEGER NOT NULL,
-        mes INTEGER NOT NULL,
-        ano INTEGER NOT NULL,
-        endereco TEXT NOT NULL,
-        cidade TEXT NOT NULL,
-        estado TEXT NOT NULL,
-        cep TEXT NOT NULL,
-        numero_telefone TEXT NOT NULL,
-        pais TEXT NOT NULL,
-        genero TEXT NOT NULL
-    );''')
-    conn.commit()
+    with sqlite3.connect('BancoDeDados.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            last_name TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            senha TEXT NOT NULL,
+            dia INTEGER NOT NULL,
+            mes INTEGER NOT NULL,
+            ano INTEGER NOT NULL,
+            endereco TEXT NOT NULL,
+            cidade TEXT NOT NULL,
+            estado TEXT NOT NULL,
+            cep TEXT NOT NULL,
+            numero_telefone TEXT NOT NULL,
+            pais TEXT NOT NULL,
+        );''')
+        conn.commit()
      
 def criar_tabela_produtos():
-    cursor = conn.cursor()
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS produtos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        produto TEXT NOT NULL UNIQUE,
-        preco REAL NOT NULL,
-        disponibilidade TEXT NOT NULL,
-        condicao TEXT NOT NULL,
-        marca TEXT NOT NULL
-    );''')
-    conn.commit()
+    with sqlite3.connect('BancoDeDados.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS produtos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            produto TEXT NOT NULL UNIQUE,
+            preco REAL NOT NULL,
+            disponibilidade TEXT NOT NULL,
+            condicao TEXT NOT NULL,
+            marca TEXT NOT NULL
+        );''')
+        conn.commit()
 
 def inserir_produto(produto, preco, disponibilidade, condicao, marca):
-    cursor = conn.cursor()
-    cursor.execute('''
-        INSERT INTO produtos (produto, preco, disponibilidade, condicao, marca)
-        VALUES (?, ?, ?, ?, ?)
-    ''', (produto, preco, disponibilidade, condicao, marca))
-    conn.commit()
+    with sqlite3.connect('BancoDeDados.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO produtos (produto, preco, disponibilidade, condicao, marca)
+            VALUES (?, ?, ?, ?, ?)
+        ''', (produto, preco, disponibilidade, condicao, marca))
+        conn.commit()
 
 def inserir_usuario(name, last_name, email, senha, dia, mes, ano, endereco, cidade, estado, cep, numero_telefone, pais):
-    cursor = conn.cursor()
-    cursor.execute(
-    "SELECT * FROM users WHERE name = ? OR email = ?",
-    (name, email)
-    )
-    user_existente = cursor.fetchone()
-    if user_existente is None:
-        cursor.execute('''
-            INSERT INTO users (name, last_name, email, senha, dia, mes, ano, endereco, cidade, estado, cep, numero_telefone, pais)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        ''', (name, last_name, email, senha, dia, mes, ano, endereco, cidade, estado, cep, numero_telefone, pais))
+    with sqlite3.connect('BancoDeDados.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+        "SELECT * FROM users WHERE name = ? OR email = ?",
+        (name, email)
+        )
+        user_existente = cursor.fetchone()
+        if user_existente is None:
+            cursor.execute('''
+                INSERT INTO users (name, last_name, email, senha, dia, mes, ano, endereco, cidade, estado, cep, numero_telefone, pais)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ''', (name, last_name, email, senha, dia, mes, ano, endereco, cidade, estado, cep, numero_telefone, pais))
         conn.commit()     
 
 def buscar_usuario():
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM users;')
-    return cursor.fetchall()
+    with sqlite3.connect('BancoDeDados.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM users;')
+        return cursor.fetchall()
+    
 
 def buscando_os_produtos():
-    cursor = conn.cursor()
-    cursor.execute('''
-    SELECT * FROM produtos;
-    ''')
-    produtos = cursor.fetchall()
-    return produtos
+    with sqlite3.connect('BancoDeDados.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+        SELECT * FROM produtos;
+        ''')
+        produtos = cursor.fetchall()
+        return produtos
 
 
--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import auth
 import Database
 import scraper
 import exporte
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 # IMPORTANTE: O código abaixo é apenas um exemplo de como você pode usar o Playwright para automatizar o login e a coleta de dados de um site. Certifique-se de ter as bibliotecas necessárias instaladas e configure corretamente o ambiente antes de executar o código.
 
 with sync_playwright() as pw:
@@ -12,14 +16,41 @@ with sync_playwright() as pw:
     page.goto("https://automationexercise.com/")
     Database.criar_tabela_produtos()
     Database.criar_tabela_user()
-    
-    Acesso =auth.login(page, "jose.silva@example.com", "password123")
+    NOME = os.getenv("NAME")
+    SOBRENOME = os.getenv("LAST_NAME")
+    EMAIL = os.getenv("EMAIL")
+    SENHA = os.getenv("SENHA")
+    DIA = os.getenv("DIA")
+    MES = os.getenv("MES")
+    ANO = os.getenv("ANO")
+    ENDERECO = os.getenv("ENDERECO")
+    CIDADE = os.getenv("CIDADE")
+    ESTADO = os.getenv("ESTADO")
+    CEP = os.getenv("CEP")
+    NUMERO_TELEFONE = os.getenv("NUMERO_TELEFONE")
+    PAIS = os.getenv("PAIS")
+
+    Acesso =auth.login(page, EMAIL, SENHA)
     if Acesso:
         print("Login successful")  
     else:
-        user = auth.register(page, "jose", "silva", "jose.silva@example.com", "password123", "1", "January", "1990", "456 Oak Ave", "Somewhere", "Somestate", "67890", "555-555-5556", "United States")
+        user = auth.register(page, NOME, SOBRENOME, EMAIL, SENHA, DIA, MES, ANO, ENDERECO, CIDADE, ESTADO, CEP, NUMERO_TELEFONE, PAIS)
         if user:
-            Database.inserir_usuario("jose", "silva", "jose.silva@example.com", "password123", "1", "January", "1990", "456 Oak Ave", "Somewhere", "Somestate", "67890", "555-555-5556", "United States")
+            Database.inserir_usuario(
+                name=NOME,
+                last_name=SOBRENOME,
+                email=EMAIL,
+                senha=SENHA,
+                dia=DIA,
+                mes=MES,
+                ano=ANO,
+                endereco=ENDERECO,
+                cidade=CIDADE,
+                estado=ESTADO,
+                cep=CEP,
+                numero_telefone=NUMERO_TELEFONE,
+                pais=PAIS
+            )
 
     dados = scraper.acessar_itens(page)
     for produto in dados:
